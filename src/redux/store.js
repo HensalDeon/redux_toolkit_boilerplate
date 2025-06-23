ddaa338d5bfb5c98d6a158a56bd01de32d544125
hensal_wac_react <hensal.d@webandcrafts.com>
2025-06-23 17:05:48 +0530

    updated slice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { mainApi } from './services/mainApi';
+import authReducer from './slices/authSlice';
 
 const store = configureStore({
     reducer: {
         [mainApi.reducerPath]: mainApi.reducer,
+        auth: authReducer,
     },
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware().concat(mainApi.middleware),
